1cf05f1f317517acbc0166eec59aa28f05755854
Shahid N. Shah <shahid@shah.org>
2020-11-03 20:11:46 -0500

    fix: add helpers to standard export

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./escape-html.ts";
 export * from "./template-literal.ts";
 export * from "./template-module.ts";
+export * from "./template-helpers.ts";
 export * from "./xml-tag.ts";
