8fad7c24125f4fc927fcf754dc6f30d7793c2421
Shahid N. Shah <shahid@shah.org>
2020-11-11 15:31:02 -0500

    fix: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export * as colors from "https://deno.land/std@0.75.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.75.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.75.0/path/mod.ts";
-export * as oak from "https://deno.land/x/oak@v6.3.1/mod.ts";
-export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.4/src/docopt.ts";
-export * as govnSvcHelpers from "https://denopkg.com/gov-suite/governed-service-helpers@v0.1.0/mod.ts";
+export * as colors from "https://deno.land/std@0.77.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.77.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.77.0/path/mod.ts";
+export * as oak from "https://deno.land/x/oak@v6.3.2/mod.ts";
+export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.5/src/docopt.ts";
+export * as govnSvcHelpers from "https://denopkg.com/gov-suite/governed-service-helpers@v0.1.1/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
-export * as interpolate from "https://denopkg.com/shah/text-interpolate@v1.0.0/mod.ts";
+export * as interpolate from "https://denopkg.com/shah/text-interpolate@v1.0.7/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.0/mod.ts";
 export * as shell from "https://denopkg.com/shah/tsd-shell@v0.9.1/mod.ts";
