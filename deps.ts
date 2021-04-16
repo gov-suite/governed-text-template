3a9e38009bcd15da2f3426848e93c466427e5d9d
Alan Francis <alan.francis@citrusinformatics.com>
2021-04-16 09:31:37 +0530

    fix: update docopt for deno 1.9.0

--- deps.ts
+++ deps.ts
@@ -2,7 +2,9 @@ export * as colors from "https://deno.land/std@0.87.0/fmt/colors.ts";
 export * as fs from "https://deno.land/std@0.87.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.87.0/path/mod.ts";
 export * as oak from "https://deno.land/x/oak@v6.5.0/mod.ts";
-export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
+//export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
+// TODO: when Eyal-Shalev/docopt.js is upgraded, replace this with latest version:
+export * as docopt from "https://denopkg.com/netspective-studios/docopt-TEMP.js/src/docopt.ts";
 export * as govnSvcHealth from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.15/health.ts";
 export * as oakHelpers from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.15/oak.ts";
 export * as govnSvcVersion from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.15/version.ts";
